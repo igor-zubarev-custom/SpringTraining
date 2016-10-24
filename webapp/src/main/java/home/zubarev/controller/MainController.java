65f1fdc861c855f2551deb0eb88044da8b19b36a
mistcode <zubarevdev@gmail.com>
2016-10-25 00:16:41 +0300

    Changes 25.10

--- webapp/src/main/java/home/zubarev/controller/MainController.java
+++ webapp/src/main/java/home/zubarev/controller/MainController.java
@@ -2,20 +2,6 @@ package home.zubarev.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
-/**
- * TODO:
- * +1) SPLIT Main controller
- * 2) multimodule project
- * +3) cart service session scope
- * +4) rename order -> cart
- * +5) http response status 400
- * +6) fix add2cart javascript
- * +7) spring tags on cart page + error per cart item
- * +8) define DB connection in project.properties
- * ___________________________
- *
- */
 @Controller
 public class MainController {
     @RequestMapping(value = "/")
